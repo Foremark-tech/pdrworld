c56817269513f6c34064117f79d0d9000b5e9d57
Amey Bauchkar <bauchkaramey1306@gmail.com>
2026-05-02 03:36:29 +0530

    Latest production-ready updates

--- archive_unused/_build_tools/find_main.ps1
+++
@@ -1,9 +0,0 @@
-foreach($f in @('about.html','products.html','solutions.html','resources.html','contact.html')){
-  $c = Get-Content $f
-  $s = 0; $e = 0
-  for($i = 0; $i -lt $c.Count; $i++){
-    if($c[$i] -match '<main'){ $s = $i + 1 }
-    if($c[$i] -match '</main>'){ $e = $i + 1 }
-  }
-  Write-Host ('{0}: main={1}-{2} total={3}' -f $f, $s, $e, $c.Count)
-}
